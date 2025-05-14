7b3773c6887aad61fa6731939ecaa746d7e5724f
accban2006 <chibao200065@gmail.com>
2025-05-14 10:06:03 +0700

    Update User.java

--- assignment/src/User.java
+++ assignment/src/User.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class User {
     protected String userID;
     protected String userName;
@@ -35,9 +38,9 @@ public class User {
     }
     public User(){
         this.userID = "u_1234567890";
-        this.userName = userName;
-        this.userPassword = userPassword;
-        this.userRegisterTime = userRegisterTime;
+        this.userName = "Default UserName";
+        this.userPassword = "Default Password";
+        this.userRegisterTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy_HH:mm:ss"));
         this.userRole = "Customer";
 
     }
